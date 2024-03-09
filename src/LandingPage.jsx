5c3d290688258ad6afff1696706204eba51ed7cd
mojeda20 <moises.liota@correo.unimet.edu.ve>
2024-03-09 01:11:35 -0400

    Landing al 70%
    Agregue el carousel que muestra los clubes en el landing page, aunque hay que acomodarlo más o menos, esta funcional

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Carousel from "./components/Carousel";
 
 function LoginPage() {
   return (
     <div className="landing-page">
       <Header />
       <Hero />
+      <Carousel />
     </div>
   );
 }
