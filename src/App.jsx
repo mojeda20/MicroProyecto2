416553b6641276761356305af25989cbee6bfd22
georginaakel <georgina.akel@correo.unimet.edu.ve>
2024-03-08 23:51:25 -0400

    update firestore y app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import LoginPage from "./LoginPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./LandingPage"; // Asegúrate de importar tu nueva Landing Page
-import ClubsList from './ClubsList';
-import VideoGamesList from './VideoGamesList';
 
 // ... otros imports
 function App() {
@@ -12,8 +10,6 @@ function App() {
         <Route path="/" element={<LoginPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/landing" element={<LandingPage />} />
-        <Route path="/clubs" element={<ClubsList />} /> 
-        <Route path="/videogames" element={<VideoGamesList />} /> 
       </Routes>
     </Router>
   );
