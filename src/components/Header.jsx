f4f82748d248309d3dcd25cb407d45318356d1fc
georginaakel <georgina.akel@correo.unimet.edu.ve>
2024-03-10 18:22:45 -0400

    footer y css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,12 @@
 import "../styles/Header.css";
+import logoImage from "../assets/logovideogames.png";
+
 const Header = () => {
   return (
     <header className="header">
-      <div className="logo">LOGO</div>
+      <div className="logo">
+        <img src={logoImage} alt="Logo" />
+      </div>
       <nav>
         <ul className="nav">
           <li className="nav-item">Buscar Clubes</li>
